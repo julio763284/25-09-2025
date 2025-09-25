18b97708bb7099f71d99743fcd352217708ddd54
julio763284 <julio763284@gmail.com>
2025-09-25 14:41:48 -0500

    Creacion de las vista Homepage y sus subvistas

--- formulario/lib/main.dart
+++ formulario/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:formulario/Presentacion/Widgets/Initial.dart';
 import 'package:formulario/Presentacion/Widgets/formulario.dart';
 import 'package:formulario/Presentacion/Widgets/loading.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 void main() => runApp(MyApp());
@@ -12,9 +13,139 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Loading(),
+        home: HomePage(),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0.0,
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+            decoration: BoxDecoration(image: DecorationImage(image: AssetImage("FondoHomepage02redi.jpg"), fit: BoxFit.cover, opacity: 0.8)),
+            )
+            ),
+          Positioned(
+            top: 20.0,
+            bottom: 20.0,
+            left: 20.0,
+            right: 20.0,
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30.0), color: Color.fromARGB(100, 0, 0, 0)),
+            child: Column(
+              children: [
+                SizedBox(height: 40.0),
+                Text("RESERVISTA # 100" , style: TextStyle(fontStyle: FontStyle.italic, fontSize: 35.0, color: Colors.white)),
+                SizedBox(height: 10.0),
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(360.0), image: DecorationImage(image: AssetImage("Reservistaredimensionado.jpg"), fit: BoxFit.cover)),
+                ),
+                SizedBox(height: 20.0),
+                HomeviewLoading()
+                ],
+            ),
+          )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomeviewInitial extends StatelessWidget {
+  const HomeviewInitial({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 300.0,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30.0), color: Color.fromARGB(200, 0, 0, 0)),
+      child: Column(
+        children: [
+          SizedBox(height: 50.0,),
+          Text("-----" , style: TextStyle(color: Colors.white, fontSize: 20.0),),
+          SizedBox(height: 50.0,),
+          Text("-----" , style: TextStyle(color: Colors.white, fontSize: 20.0),),
+          SizedBox(height: 50.0,),
+          Text("-----" , style: TextStyle(color: Colors.white, fontSize: 20.0),)
+        ],
+      ),
+    );
+  }
+}
+
+class HomeviewSuccess extends StatelessWidget {
+  const HomeviewSuccess({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 300.0,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30.0), color: Color.fromARGB(200, 0, 0, 0)),
+      child: Column(
+        children: [
+          SizedBox(height: 50.0,),
+          Text("Nombre Api" , style: TextStyle(color: Colors.white, fontSize: 20.0),),
+          SizedBox(height: 50.0,),
+          Text("Nacionalidad Api" , style: TextStyle(color: Colors.white, fontSize: 20.0),),
+          SizedBox(height: 50.0,),
+          Text("Edad Api" , style: TextStyle(color: Colors.white, fontSize: 20.0),)
+        ],
+      ),
+    );
+  }
+}
+
+class HomeviewLoading extends StatelessWidget {
+  const HomeviewLoading({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 300.0,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30.0), color: Color.fromARGB(200, 0, 0, 0)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Cargando..." , style: TextStyle(color: Colors.white, fontSize: 30.0)),
+          SizedBox(height: 50.0,),
+          Container(
+            child: SizedBox(
+              height: 80,
+              width: 80,
+              child: CircularProgressIndicator(color: Colors.white,),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
 
 
