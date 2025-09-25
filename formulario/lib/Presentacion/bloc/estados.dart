64be32e642c6796a5b6d39f1f73d72595a0f923e
julio763284 <julio763284@gmail.com>
2025-09-25 14:49:13 -0500

    Creacion de los estados y eventos de la aplicacion

---
+++ formulario/lib/Presentacion/bloc/estados.dart
@@ -0,0 +1,5 @@
+class Estadosapp {}
+class EstadoInicialapp extends Estadosapp{}
+class EstadoLoadingapp extends Estadosapp{}
+class EstadoFallidoapp extends Estadosapp{}
+class EstadoExitosoapp extends Estadosapp{}
---
+++ formulario/lib/Presentacion/bloc/eventos.dart
@@ -0,0 +1,2 @@
+class Eventosapp {}
+class Iniciarapp extends Eventosapp {}
